3eef700059f4b877e08518b26029e67f4bf14e69
ngocnguyen282 <139733587+ngocnguyen282@users.noreply.github.com>
2024-10-09 15:10:09 -0400

    modified:   main.py modified:   static/styles.css new file:   templates/index.html

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
