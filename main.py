0b026e2c753180f1652e2cbb7699eeb950af62f3
Josephine Zundel <josephine.zundel@gmail.com>
2024-10-09 16:25:17 -0400

    uncommented

--- main.py
+++ main.py
@@ -53,51 +53,51 @@ def signUp():
     # Render the HTML template
     return render_template("signUp.html")
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user and user.password == form.password.data:
-#             flash(f'Logged in successfully as {form.username.data}!', 'success')
-#             return redirect(url_for('home'))
-#         else:
-#             flash('Login Unsuccessful. Please check username and password.', 'danger')
-#     return render_template('logIn.html', form=form)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            flash(f'Logged in successfully as {form.username.data}!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password.', 'danger')
+    return render_template('logIn.html', form=form)
 
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit(): # don't need constructor in User() since sqlalchemy has deafult one
-#         new_user = User(username=form.username.data, password=form.password.data)
-#         db.session.add(new_user)
-#         db.session.commit()
-#         flash(f'Account created for {form.username.data}!', 'success')
-#         return redirect(url_for('login'))
-#     return render_template('register.html', form=form)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # don't need constructor in User() since sqlalchemy has deafult one
+        new_user = User(username=form.username.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
-# @app.route('/tutors')
-# def tutors():
-#     # fetch all tutors from the database (query)
-#     all_tutors = Tutor.query.all()
-#     return render_template('tutors.html', tutors=all_tutors)
+@app.route('/tutors')
+def tutors():
+    # fetch all tutors from the database (query)
+    all_tutors = Tutor.query.all()
+    return render_template('tutors.html', tutors=all_tutors)
 
-# def add_dummy_data():
-#     # if there are no tutors in the database -> add dummy data
-#     if Tutor.query.count() == 0:
-#         dummy_tutors = [
-#             Tutor(name='Joe Cool', subject='Math', rating=4.9),
-#             Tutor(name='Coco Melon', subject='Physics', rating=4.8),
-#             Tutor(name='Cookie Dough', subject='Chemistry', rating=4.7),
-#             Tutor(name='Alice Wonderland', subject='Biology', rating=4.6)
-#         ]  
-#         db.session.bulk_save_objects(dummy_tutors)
-#         db.session.commit()
-#         print("Dummy tutor data has been added!")
+def add_dummy_data():
+    # if there are no tutors in the database -> add dummy data
+    if Tutor.query.count() == 0:
+        dummy_tutors = [
+            Tutor(name='Joe Cool', subject='Math', rating=4.9),
+            Tutor(name='Coco Melon', subject='Physics', rating=4.8),
+            Tutor(name='Cookie Dough', subject='Chemistry', rating=4.7),
+            Tutor(name='Alice Wonderland', subject='Biology', rating=4.6)
+        ]  
+        db.session.bulk_save_objects(dummy_tutors)
+        db.session.commit()
+        print("Dummy tutor data has been added!")
 
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()  # create all tables in the database
-#         add_dummy_data()  # if the Tutor table is empty add dummy data
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # create all tables in the database
+        add_dummy_data()  # if the Tutor table is empty add dummy data
 
-#     app.run(debug=True)
+    app.run(debug=True)
